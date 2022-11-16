701cba3fd241eb473b8f7270be66e4a2b0cddc5f
Samy Rjob <s.rjob@audencia.com>
2022-11-16 16:30:56 +0100

    yeah

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,8 +11,11 @@ before_action :set_restaurant, only: %i[new create]
   def create
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
-    @review.save
+    if @review.save
     redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end 
   end
 
   private
